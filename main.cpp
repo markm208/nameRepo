50b06d37558fe1aa17958b809203d09cffca9e2d
Mark Mahoney <markm208@gmail.com>
2025-03-04 13:59:47 -0600

    Added a function to print my name.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void printName();
+
 int main()
 {
-    cout<<"GitHub Demo"<<endl;
+    printName();
     return 0;
+}
+//--
+void printName()
+{
+    cout<<"Mark Mahoney"<<endl;
 }
